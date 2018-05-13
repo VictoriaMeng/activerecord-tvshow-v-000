31f37e714072d4546b9f82e559b4c26828a97ae8
Victoria Meng <nights1ststar@protonmail.com>
2018-05-13 23:08:34 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,4 +6,11 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     Show.where(rating: highest_rating).first
   end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+  end
 end
